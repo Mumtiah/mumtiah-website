15a52f6edae83928076d33d56756f8c48dda3914
Joye Shonubi <joye@goforward.com>
2023-09-08 00:20:50 +0100

    added programs data

---
+++ src/app/components/programs/ProgramCard.js
@@ -0,0 +1,55 @@
+import { Box, Grid, AspectRatio, Text, Button } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+
+export const ProgramCard = ({
+  media,
+  isVideo = false,
+  heroText,
+  sublineText,
+}) => {
+  return (
+    <Grid
+      templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+      alignItems="center"
+      width="full"
+      p={5}
+      height="476px"
+    >
+      {/* Media Section */}
+      <Box>
+        {isVideo ? (
+          // You would need to replace the iframe `src` with a link to your video, or dynamically pass it in as props
+          <AspectRatio ratio={16 / 9}>
+            <iframe
+              src={media}
+              title="Embedded Youtube Video"
+              allowFullScreen
+            />
+          </AspectRatio>
+        ) : (
+          <img width={"100%"} height="100%" src={media} alt="Image Alt" />
+        )}
+      </Box>
+
+      {/* Text Section */}
+
+      <Box bg="#CCF6DD" height={"100%"} p={4}>
+        <Text fontSize="2xl" fontWeight="semibold" mb={3}>
+          {heroText}
+        </Text>
+        <Text fontSize="lg" mb={5}>
+          {sublineText}
+        </Text>
+        <Button
+          rightIcon={<ArrowForwardIcon />}
+          colorScheme="teal"
+          variant="outline"
+        >
+          Apply
+        </Button>
+      </Box>
+    </Grid>
+  );
+};
+
+// export ProgramCard;
--- src/app/programs/page.js
+++ src/app/programs/page.js
@@ -1,3 +1,55 @@
+"use client";
+
+import { ProgramCard } from "../components/programs/ProgramCard";
+
+const programData = [
+  {
+    heroText: "Virtual Classes",
+    sublineText: "We teach based on what Allah and his prophet commanded",
+    isVideo: true,
+    media: "https://www.youtube.com/embed/NSoX4ZJ2v5o?si=oRJ8aXwDQCs-Ugnv",
+  },
+  {
+    heroText: "Free online class",
+    sublineText: "We teach based on foundational principles",
+    isVideo: true,
+    media: "https://www.youtube.com/embed/zBjNiLV_HaY?si=E6t87XZEkBa9JQCE", // replace with your Youtube embed video link
+  },
+  {
+    heroText: "Physical class",
+    sublineText: "Experience personal interaction and community",
+    isVideo: true,
+    media: "https://www.youtube.com/embed/TyFDedjz8HE?si=2PdXC5Ji9PYi6kA1", // replace with your Youtube embed video link
+  },
+  {
+    heroText: "Ramadan lecture",
+    sublineText: "Gain beneficial knowledge this Ramadan",
+    isVideo: false,
+    media:
+      "https://muslimaid.storage.googleapis.com/upload/img_cache/file-18913-2d11cab24467c01f935f83fa8ec58bf8.jpg", // replace with your image link
+  },
+
+  {
+    heroText: "Mumtiah experience",
+    sublineText: "Discover the Mumtiah experience",
+    isVideo: false,
+    media:
+      "https://muslimaid.storage.googleapis.com/upload/img_cache/file-18913-2d11cab24467c01f935f83fa8ec58bf8.jpg",
+  },
+];
+
 export default function Programs() {
-  return <div>This is the Programs page</div>;
+  return (
+    <div>
+      {programData.map((program, index) => (
+        <ProgramCard
+          key={index}
+          heroText={program.heroText}
+          sublineText={program.sublineText}
+          isVideo={program.isVideo}
+          media={program.media}
+        />
+      ))}
+    </div>
+  );
 }
