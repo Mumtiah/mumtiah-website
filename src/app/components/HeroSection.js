171cd338ce41f8db63de7d2ba475723c45348ff9
Abdulqoyum6 <abdulqoyum@simplifiedux.com>
2023-09-08 17:17:41 +0100

    initial commit

---
+++ src/app/components/HeroSection.js
@@ -0,0 +1,14 @@
+"use-client"
+
+import { Container, Text } from "@chakra-ui/react";
+
+export const HeroSection = () => {
+    <Container>
+        <Text 
+        display="block"
+        fontSize="lg"
+        lineHeight="normal"
+        fontWeight="semibold">MUMTIAH</Text>
+        <Text>Welcome to Our Arabic & Islamic Studies School: Where Tradition Meets Enlightenment, And Knowledge Flourishes with Spiritual Insight.</Text>
+    </Container>
+}
