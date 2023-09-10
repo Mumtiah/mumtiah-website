a54fe033276f5a0e89a144fe1ed2362c5e9a3158
Joye Shonubi <joye@goforward.com>
2023-09-10 08:45:14 +0100

    update link import

--- src/app/components/navigation/MobileDrawer.js
+++ src/app/components/navigation/MobileDrawer.js
@@ -6,8 +6,11 @@ import {
   DrawerContent,
   Stack,
   useDisclosure,
+  HStack,
 } from "@chakra-ui/react";
 import { ToggleButton } from "./ToggleButton";
+import { CloseIcon } from "@chakra-ui/icons";
+import Link from "next/link";
 
 export const MobileDrawer = () => {
   const { isOpen, onToggle, onClose } = useDisclosure();
@@ -25,32 +28,42 @@ export const MobileDrawer = () => {
       <Drawer placement="top" isOpen={isOpen} onClose={onClose}>
         <DrawerContent bg="#FEFFED" h={"100vh"} w="100vw">
           <DrawerHeader>
-            <ToggleButton
-              isOpen={isOpen}
-              onClick={onToggle}
-              aria-label="Open menu"
-            />
+            <HStack>
+              <ToggleButton
+                isOpen={isOpen}
+                onClick={onToggle}
+                aria-label="Open menu"
+              >
+                Menu
+              </ToggleButton>
+              <CloseIcon />
+            </HStack>
           </DrawerHeader>
 
           <DrawerBody mt="72px" p="4">
-            <Stack spacing="1">
+            <Stack spacing="4">
+              <Link href="/">
+                <Text fontSize="14px" fontWeight="400">
+                  Homepage
+                </Text>
+              </Link>
               <Link href="/programs">
-                <Text fontSize="20px" fontWeight="600">
+                <Text fontSize="14px" fontWeight="400">
                   Our Program
                 </Text>
               </Link>
               <Link href="/enrolment">
-                <Text fontSize="20px" fontWeight="600">
+                <Text fontSize="14px" fontWeight="400">
                   Enroll
                 </Text>
               </Link>
               <Link href="/donate">
-                <Text fontSize="20px" fontWeight="600">
+                <Text fontSize="14px" fontWeight="400">
                   Donate
                 </Text>
               </Link>
               <Link href="/alumni">
-                <Text fontSize="20px" fontWeight="600">
+                <Text fontSize="14px" fontWeight="400">
                   Alumni
                 </Text>
               </Link>
