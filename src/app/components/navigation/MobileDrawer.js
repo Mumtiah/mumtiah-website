3018c2ef31c340661998ff3aa833714af3d5f9b1
Joye Shonubi <joye@goforward.com>
2023-09-10 06:36:31 +0100

    update mobile nav

--- src/app/components/navigation/MobileDrawer.js
+++ src/app/components/navigation/MobileDrawer.js
@@ -1,12 +1,12 @@
 import {
   Button,
   Drawer,
+  DrawerHeader,
   DrawerBody,
   DrawerContent,
   Stack,
   useDisclosure,
 } from "@chakra-ui/react";
-import { DocumentCollapse } from "./DocumentCollapse";
 import { ToggleButton } from "./ToggleButton";
 
 export const MobileDrawer = () => {
@@ -23,19 +23,37 @@ export const MobileDrawer = () => {
         }}
       />
       <Drawer placement="top" isOpen={isOpen} onClose={onClose}>
-        <DrawerContent>
+        <DrawerContent bg="#FEFFED" h={"100vh"}>
+          <DrawerHeader>
+            <ToggleButton
+              isOpen={isOpen}
+              onClick={onToggle}
+              aria-label="Open menu"
+            />
+          </DrawerHeader>
+
           <DrawerBody mt="72px" p="4">
             <Stack spacing="1">
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                Dashboard
-              </Button>
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                Analysis
-              </Button>
-              <DocumentCollapse />
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                History
-              </Button>
+              <Link href="/programs">
+                <Text fontSize="20px" fontWeight="600">
+                  Our Program
+                </Text>
+              </Link>
+              <Link href="/enrolment">
+                <Text fontSize="20px" fontWeight="600">
+                  Enroll
+                </Text>
+              </Link>
+              <Link href="/donate">
+                <Text fontSize="20px" fontWeight="600">
+                  Donate
+                </Text>
+              </Link>
+              <Link href="/alumni">
+                <Text fontSize="20px" fontWeight="600">
+                  Alumni
+                </Text>
+              </Link>
             </Stack>
           </DrawerBody>
         </DrawerContent>
