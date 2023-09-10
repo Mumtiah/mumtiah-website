7e46ae32b90c268e861ee23d2d9808d074da12b7
Joye Shonubi <38365007+Oladunjoye@users.noreply.github.com>
2023-09-10 08:59:56 +0100

    update mobile nav (#19)
    * update mobile nav
    
    * update width
    
    * update link import
    
    * update imports
    
    ---------
    
    Co-authored-by: Joye Shonubi <joye@goforward.com>

--- src/app/components/navigation/MobileDrawer.js
+++ src/app/components/navigation/MobileDrawer.js
@@ -1,13 +1,17 @@
 import {
+  Text,
   Button,
   Drawer,
+  DrawerHeader,
   DrawerBody,
   DrawerContent,
   Stack,
   useDisclosure,
+  HStack,
 } from "@chakra-ui/react";
-import { DocumentCollapse } from "./DocumentCollapse";
 import { ToggleButton } from "./ToggleButton";
+import { CloseIcon } from "@chakra-ui/icons";
+import Link from "next/link";
 
 export const MobileDrawer = () => {
   const { isOpen, onToggle, onClose } = useDisclosure();
@@ -23,19 +27,47 @@ export const MobileDrawer = () => {
         }}
       />
       <Drawer placement="top" isOpen={isOpen} onClose={onClose}>
-        <DrawerContent>
+        <DrawerContent bg="#FEFFED" h={"100vh"} w="100vw">
+          <DrawerHeader>
+            <HStack>
+              <ToggleButton
+                isOpen={isOpen}
+                onClick={onToggle}
+                aria-label="Open menu"
+              >
+                Menu
+              </ToggleButton>
+              <CloseIcon />
+            </HStack>
+          </DrawerHeader>
+
           <DrawerBody mt="72px" p="4">
-            <Stack spacing="1">
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                Dashboard
-              </Button>
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                Analysis
-              </Button>
-              <DocumentCollapse />
-              <Button size="lg" variant="tertiary" justifyContent="start">
-                History
-              </Button>
+            <Stack spacing="4">
+              <Link href="/">
+                <Text fontSize="14px" fontWeight="400">
+                  Homepage
+                </Text>
+              </Link>
+              <Link href="/programs">
+                <Text fontSize="14px" fontWeight="400">
+                  Our Program
+                </Text>
+              </Link>
+              <Link href="/enrolment">
+                <Text fontSize="14px" fontWeight="400">
+                  Enroll
+                </Text>
+              </Link>
+              <Link href="/donate">
+                <Text fontSize="14px" fontWeight="400">
+                  Donate
+                </Text>
+              </Link>
+              <Link href="/alumni">
+                <Text fontSize="14px" fontWeight="400">
+                  Alumni
+                </Text>
+              </Link>
             </Stack>
           </DrawerBody>
         </DrawerContent>
